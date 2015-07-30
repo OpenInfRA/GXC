1f184e40012edf765d5b16493e28c9bbd681023c
Benjamin Thurm <bentrm@posteo.de>
2015-07-30 10:04:50 +0200

    Remove Deft override

--- overrides/InjectableComponent.js
+++
@@ -1,8 +0,0 @@
-Ext.define('GXC.InjectableComponent', {
-  override: 'Ext.Component',
-  constructor: function(config) {
-    config = Ext.apply(config || {}, this.injectConfig);
-    delete this.injectConfig;
-    return this.callSuper([config]);
-  }
-});
--- overrides/Readme.md
+++
@@ -1,3 +0,0 @@
-# GXC/overrides
-
-This folder contains overrides which will automatically be required by package users.
